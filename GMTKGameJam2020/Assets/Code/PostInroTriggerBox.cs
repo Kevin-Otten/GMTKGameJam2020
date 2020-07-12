131887a2f2497b4a2bea74122c07fa67d671d344
KevinOtten <kevin.otten@ziggo.nl>
2020-07-12 13:38:26 +0200

    fixed trigger hitbox en intro hitbox code

---
+++ GMTKGameJam2020/Assets/Code/PostInroTriggerBox.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostInroTriggerBox : MonoBehaviour
+{
+    public CameraMovement cameraMovement;
+    public Rigidbody2D playerRigidbody;
+
+    private bool triggerd;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(!triggerd)
+        {
+            cameraMovement.Smoothness = 5;
+            playerRigidbody.gravityScale = 3;
+        }
+    }
+}
